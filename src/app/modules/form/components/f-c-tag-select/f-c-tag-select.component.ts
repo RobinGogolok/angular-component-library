4d73ed34d05afed63ad2a91a1c557eeef059f428
RobinGogolok <privat@robingogolok.de>
2019-02-05 20:44:18 +0100

    Added IE and Edge fix Auto format Added --prod flag to build script in package.json

--- src/app/modules/form/components/f-c-tag-select/f-c-tag-select.component.ts
+++ src/app/modules/form/components/f-c-tag-select/f-c-tag-select.component.ts
@@ -18,24 +18,24 @@ export interface FilteredSelectItem {
 } )
 export class FCTagSelectComponent implements OnInit, OnDestroy {
 
-  @ViewChild("searchInput") searchInputRef: ElementRef;
-  @ViewChild("searchAutocompleteWrap") searchAutocompleteWrapRef: ElementRef;
+  @ViewChild( 'searchInput' ) searchInputRef: ElementRef;
+  @ViewChild( 'searchAutocompleteWrap' ) searchAutocompleteWrapRef: ElementRef;
 
-  @Input() filterFunction: (searchInput: string) => Observable<SelectItem[]>;
+  @Input() filterFunction: ( searchInput: string ) => Observable<SelectItem[]>;
 
   @Input() maxItems = 10;
 
+  public formGroup: FormGroup;
   public isSearchAutocompleteOpen = false;
   public filteredItems: FilteredSelectItem[] = [];
+  public selectedItems: SelectItem[] = [];
 
   private filteredItemFocusIndex: number | null = null;
-  private selectedItems: SelectItem[] = [];
   private windowEventHandler: EventListener;
   private noPointerEventsTimeOut;
 
-  private formGroup: FormGroup;
 
-  constructor(private fb: FormBuilder) {
+  constructor( private fb: FormBuilder ) {
   }
 
   ngOnInit() {
@@ -53,97 +53,96 @@ export class FCTagSelectComponent implements OnInit, OnDestroy {
 
   public onSearchInputInput( event ): void {
     this.filteredItemFocusIndex = null;
-    if (event.target === undefined && event.target.value === undefined) {
-      if (!environment.production) {
-        console.warn('Can\'t read target input value!', event);
+    if ( event.target === undefined && event.target.value === undefined ) {
+      if ( !environment.production ) {
+        console.warn( 'Can\'t read target input value!', event );
       }
       return;
     }
     const searchInput = event.target.value.toLowerCase();
-    if (searchInput === '') {
+    if ( searchInput === '' ) {
       this.clearFilteredItems();
       return;
     }
-    this.filterItems(searchInput);
+    this.filterItems( searchInput );
   }
 
   public onSearchInputFocus(): void {
     this.openSearchAutocomplete();
   }
 
-  public onSearchInputKeyDown( event: KeyboardEvent): void {
-    switch ( event.code ) {
-      case 'ArrowDown': {
-        if ( this.filteredItems.length === 0 ) {
-          return;
-        }
-        if (this.filteredItemFocusIndex === null) {
-          this.filteredItemsFocusNext();
-        } else if (this.filteredItemFocusIndex === this.filteredItems.length -1) {
-        } else {
-          this.filteredItemsFocusNext();
-        }
-        this.setFocusToFilteredItem(this.filteredItemFocusIndex);
-        this.scrollItemIntoView(this.filteredItemFocusIndex);
-        break;
+  public onSearchInputKeyDown( event: KeyboardEvent ): void {
+    let code = '';
+
+    if ( event.code !== undefined ) {
+      code = event.code;
+    } else {
+      code = event.key;
+    }
+
+    if ( code === 'ArrowDown' || code === 'Down' ) {
+      if ( this.filteredItems.length === 0 ) {
+        return;
       }
-      case 'ArrowUp': {
-        if ( this.filteredItems.length === 0 ) {
-          return;
-        }
-        if (this.filteredItemFocusIndex === null) {
-          return;
-        } else if (this.filteredItemFocusIndex === 0) {
-          this.filteredItemFocusIndex = null;
-          const searchValue = this.getSearchInputValue();
-          this.patchValue('');
-          this.patchValue(searchValue);
-        } else {
-          this.filteredItemsFocusPrevious();
-        }
-        this.setFocusToFilteredItem(this.filteredItemFocusIndex);
-        this.scrollItemIntoView(this.filteredItemFocusIndex);
-        break;
+      if ( this.filteredItemFocusIndex === null ) {
+        this.filteredItemsFocusNext();
+      } else if ( this.filteredItemFocusIndex === this.filteredItems.length - 1 ) {
+      } else {
+        this.filteredItemsFocusNext();
       }
-      case 'Enter': {
-        if ( this.filteredItems.length === 0 ) {
-          return;
-        }
-        if (this.filteredItemFocusIndex === null) {
-          break;
-        }
-        const item = this.filteredItems[this.filteredItemFocusIndex];
-        if (!this.isItemSelected(item)) {
-          this.addItemToSelectedItems(item);
-          this.filteredItemFocusIndex = null;
-          this.clearSearchInput();
-          this.clearFilteredItems();
-        }
-        break;
+      this.setFocusToFilteredItem( this.filteredItemFocusIndex );
+      this.scrollItemIntoView( this.filteredItemFocusIndex );
+    } else if ( code === 'ArrowUp' || code === 'Up' ) {
+      if ( this.filteredItems.length === 0 ) {
+        return;
       }
-      case 'Escape': {
-        if (this.getSearchInputValue() !== '') {
+      if ( this.filteredItemFocusIndex === null ) {
+        return;
+      } else if ( this.filteredItemFocusIndex === 0 ) {
+        this.filteredItemFocusIndex = null;
+        const searchValue = this.getSearchInputValue();
+        this.patchValue( '' );
+        this.patchValue( searchValue );
+      } else {
+        this.filteredItemsFocusPrevious();
+      }
+      this.setFocusToFilteredItem( this.filteredItemFocusIndex );
+      this.scrollItemIntoView( this.filteredItemFocusIndex );
+    } else if ( code === 'Enter' ) {
+      if (
+        this.filteredItems.length === 0
+        || this.filteredItemFocusIndex === null
+      ) {
+        return;
+      }
+      const item = this.filteredItems[ this.filteredItemFocusIndex ];
+      if ( !this.isItemSelected( item ) ) {
+        this.addItemToSelectedItems( item );
         this.filteredItemFocusIndex = null;
         this.clearSearchInput();
         this.clearFilteredItems();
-        }
-        break;
+      }
+    } else if ( code === 'Escape' ) {
+      if ( this.getSearchInputValue() !== '' ) {
+        this.filteredItemFocusIndex = null;
+        this.clearSearchInput();
+        this.clearFilteredItems();
       }
     }
   }
 
   public onItemClick( item: SelectItem ): void {
     this.setFocusToSearchInput();
-    if (!this.isItemSelected(item)) {
+    if ( !this.isItemSelected( item ) ) {
       this.addItemToSelectedItems( item );
       this.clearSearchInput();
       this.clearFilteredItems();
     }
   }
 
-  public onItemMouseEnter(item: FilteredSelectItem): void {
-    this.filteredItemFocusIndex = this.filteredItems.indexOf(item);
-    this.setFocusToFilteredItem(this.filteredItemFocusIndex);
+  public onItemMouseEnter( item: FilteredSelectItem ): void {
+    this.filteredItemFocusIndex = this.filteredItems.indexOf( item );
+    this.setFocusToFilteredItem( this.filteredItemFocusIndex );
   }
 
   public onClearSearchInputClick(): void {
@@ -156,10 +155,10 @@ export class FCTagSelectComponent implements OnInit, OnDestroy {
     return this.formGroup.value.searchInput.length === 0;
   }
 
-  public isItemSelected(item: SelectItem): boolean {
-    return this.selectedItems.some(selectedItem => {
-      return selectedItem.label ===  item.label && selectedItem.value === item.value;
-    });
+  public isItemSelected( item: SelectItem ): boolean {
+    return this.selectedItems.some( selectedItem => {
+      return selectedItem.label === item.label && selectedItem.value === item.value;
+    } );
   }
 
   private filteredItemsFocusNext(): void {
@@ -168,13 +167,13 @@ export class FCTagSelectComponent implements OnInit, OnDestroy {
     }
     let loop = true;
     let index = this.filteredItemFocusIndex;
-    if (index === null) {
+    if ( index === null ) {
       index = -1;
     }
-    while(loop) {
-      const nextItem = this.filteredItems[index + 1];
-      if (typeof nextItem !== 'undefined') {
-        if (!this.isItemSelected(nextItem)) {
+    while ( loop ) {
+      const nextItem = this.filteredItems[ index + 1 ];
+      if ( typeof nextItem !== 'undefined' ) {
+        if ( !this.isItemSelected( nextItem ) ) {
           index++;
           loop = false;
         } else {
@@ -188,30 +187,30 @@ export class FCTagSelectComponent implements OnInit, OnDestroy {
     this.filteredItemFocusIndex = index;
   }
 
-  private scrollItemIntoView(itemIndex: number): void {
-    if (itemIndex === null) {
+  private scrollItemIntoView( itemIndex: number ): void {
+    if ( itemIndex === null ) {
       return;
     }
     const scrollContainerEl = this.searchAutocompleteWrapRef.nativeElement as HTMLElement;
-    const itemEl = scrollContainerEl.getElementsByClassName('item')[itemIndex] as HTMLElement;
+    const itemEl = scrollContainerEl.getElementsByClassName( 'item' )[ itemIndex ] as HTMLElement;
     const itemHeight = itemEl.clientHeight;
 
-    if (!scrollContainerEl.classList.contains('no-pointer-events')) {
-      scrollContainerEl.classList.add('no-pointer-events');
+    if ( !scrollContainerEl.classList.contains( 'no-pointer-events' ) ) {
+      scrollContainerEl.classList.add( 'no-pointer-events' );
     }
 
-    if (itemEl.offsetTop + itemHeight > scrollContainerEl.clientHeight + scrollContainerEl.scrollTop) {
+    if ( itemEl.offsetTop + itemHeight > scrollContainerEl.clientHeight + scrollContainerEl.scrollTop ) {
       scrollContainerEl.scrollTop += itemHeight;
-    } else if (itemEl.offsetTop < scrollContainerEl.scrollTop) {
+    } else if ( itemEl.offsetTop < scrollContainerEl.scrollTop ) {
       scrollContainerEl.scrollTop -= itemHeight;
     }
 
-    if (typeof this.noPointerEventsTimeOut === 'number') {
-      clearTimeout(this.noPointerEventsTimeOut);
+    if ( typeof this.noPointerEventsTimeOut === 'number' ) {
+      clearTimeout( this.noPointerEventsTimeOut );
     }
-    this.noPointerEventsTimeOut = setTimeout(() => {
-      scrollContainerEl.classList.remove('no-pointer-events');
-    }, 400);
+    this.noPointerEventsTimeOut = setTimeout( () => {
+      scrollContainerEl.classList.remove( 'no-pointer-events' );
+    }, 400 );
   }
 
   private filteredItemsFocusPrevious(): void {
@@ -220,10 +219,10 @@ export class FCTagSelectComponent implements OnInit, OnDestroy {
     }
     let loop = true;
     let index = this.filteredItemFocusIndex;
-    while(loop) {
-      const nextItem = this.filteredItems[index - 1];
-      if (typeof nextItem !== 'undefined') {
-        if (!this.isItemSelected(nextItem)) {
+    while ( loop ) {
+      const nextItem = this.filteredItems[ index - 1 ];
+      if ( typeof nextItem !== 'undefined' ) {
+        if ( !this.isItemSelected( nextItem ) ) {
           index--;
           loop = false;
         } else {
@@ -238,57 +237,57 @@ export class FCTagSelectComponent implements OnInit, OnDestroy {
   }
 
   private initFormGroup(): void {
-    this.formGroup = this.fb.group({
-      searchInput: ['']
-    });
+    this.formGroup = this.fb.group( {
+      searchInput: [ '' ]
+    } );
   }
 
-  private addItemToSelectedItems(item: SelectItem): void {
-    this.selectedItems.push(item);
+  private addItemToSelectedItems( item: SelectItem ): void {
+    this.selectedItems.push( item );
   }
 
-  private removeItemFromSelectedItems(item: SelectItem): void {
-    const selectedItemIndex = this.selectedItems.indexOf(item);
-    if (selectedItemIndex === -1) {
-      if (!environment.production) {
-        console.warn(`Can't find item "${item.label}" in selectedItems array!`);
+  private removeItemFromSelectedItems( item: SelectItem ): void {
+    const selectedItemIndex = this.selectedItems.indexOf( item );
+    if ( selectedItemIndex === -1 ) {
+      if ( !environment.production ) {
+        console.warn( `Can't find item "${item.label}" in selectedItems array!` );
       }
     }
-    this.selectedItems.splice(selectedItemIndex, 1);
+    this.selectedItems.splice( selectedItemIndex, 1 );
   }
 
-  private filterItems(searchInput: string): void {
-    this.filterFunction(searchInput).pipe(untilComponentDestroyed(this)).subscribe(
+  private filterItems( searchInput: string ): void {
+    this.filterFunction( searchInput ).pipe( untilComponentDestroyed( this ) ).subscribe(
       res => {
-        const filteredItems = this.transformItemsToFilteredItems(res);
-        if (filteredItems.length > this.maxItems) {
+        const filteredItems = this.transformItemsToFilteredItems( res );
+        if ( filteredItems.length > this.maxItems ) {
           this.filteredItems = this.transformItemsToFilteredItems(
             filteredItems.slice( 0, this.maxItems )
           );
         } else {
           this.filteredItems = filteredItems;
         }
-        this.markSearchInput(searchInput);
+        this.markSearchInput( searchInput );
       }, err => {
-        if (!environment.production) {
-          console.error(err);
+        if ( !environment.production ) {
+          console.error( err );
         }
       }
     );
   }
 
-  private markSearchInput(searchInput: string): void {
+  private markSearchInput( searchInput: string ): void {
     this.filteredItems.forEach(
       filteredItem => {
-        const start = filteredItem.label.toLowerCase().indexOf(searchInput);
+        const start = filteredItem.label.toLowerCase().indexOf( searchInput );
         const end = start + searchInput.length + 1;
-        const labelArray = filteredItem.label.split('');
+        const labelArray = filteredItem.label.split( '' );
         labelArray
-          .splice(start, 0, '<strong>');
+          .splice( start, 0, '<strong>' );
         labelArray
-          .splice(end, 0, '</strong>');
+          .splice( end, 0, '</strong>' );
 
-        filteredItem.label = labelArray.join('');
+        filteredItem.label = labelArray.join( '' );
       }
     );
   }
@@ -298,13 +297,13 @@ export class FCTagSelectComponent implements OnInit, OnDestroy {
   }
 
   private clearSearchInput(): void {
-    this.patchValue('');
+    this.patchValue( '' );
   }
 
-  private patchValue(value: string): void {
-    this.formGroup.patchValue({
+  private patchValue( value: string ): void {
+    this.formGroup.patchValue( {
       searchInput: value
-    })
+    } )
   }
 
   private getSearchInputValue(): string {
@@ -315,7 +314,7 @@ export class FCTagSelectComponent implements OnInit, OnDestroy {
     this.searchInputRef.nativeElement.focus();
   }
 
-  private transformItemToFilteredItem(item: SelectItem): FilteredSelectItem {
+  private transformItemToFilteredItem( item: SelectItem ): FilteredSelectItem {
     return {
       label: item.label,
       value: item.value,
@@ -323,38 +322,38 @@ export class FCTagSelectComponent implements OnInit, OnDestroy {
     }
   }
 
-  private transformItemsToFilteredItems(items: SelectItem[]): FilteredSelectItem[] {
-    return items.map(item => this.transformItemToFilteredItem(item));
+  private transformItemsToFilteredItems( items: SelectItem[] ): FilteredSelectItem[] {
+    return items.map( item => this.transformItemToFilteredItem( item ) );
   }
 
-  private setFocusToFilteredItem(itemIndex: number | null): void {
+  private setFocusToFilteredItem( itemIndex: number | null ): void {
     if ( this.filteredItems.length === 0 ) {
       return;
     }
-    this.filteredItems.forEach((item, index) => {
-      if (item.focus) {
+    this.filteredItems.forEach( ( item, index ) => {
+      if ( item.focus ) {
         item.focus = false;
       }
-      if (itemIndex === index) {
+      if ( itemIndex === index ) {
         item.focus = true;
       }
-    });
+    } );
   }
 
   private initWindowEventHandler(): void {
-    this.windowEventHandler = (event) => {
+    this.windowEventHandler = ( event ) => {
       const targetElement = event.target as HTMLElement;
-      if (!targetElement || typeof targetElement.classList === undefined) {
+      if ( !targetElement || typeof targetElement.classList === undefined ) {
         return;
       }
-      if (!targetElement.classList.contains('close-not-on-click')) {
+      if ( !targetElement.classList.contains( 'close-not-on-click' ) ) {
         this.closeSearchAutocomplete();
       }
     };
-    window.addEventListener('click', this.windowEventHandler);
+    window.addEventListener( 'click', this.windowEventHandler );
   }
 
   ngOnDestroy(): void {
-    window.removeEventListener('click', this.windowEventHandler);
+    window.removeEventListener( 'click', this.windowEventHandler );
   }
 }
